fdffd7b1ad9dd8ee51b71b2ea222e5b385535dc7
Franco Fraiz <franco99ariel@gmail.com>
2023-10-21 20:19:47 -0300

    Se agregan archivos js para cada html

---
+++ Js/publicarReceta.js
@@ -0,0 +1,106 @@
+var listadoIngredientes = [];
+var auth = {};
+var username = window.localStorage.getItem("username");
+var password = window.localStorage.getItem("password");
+
+console.log("Username from localStorage:", username);
+console.log("Password from localStorage:", password);
+function createArrayToPicture(array) {
+  const newArray = array.map((item) => {
+    return { url: item };
+  });
+  return newArray;
+}
+function postRecipe(ev, from) {
+  var picturesInput = document.querySelector(
+    'input[name="pictures"]'
+  ).value;
+  var title = document.querySelector('input[name="title"]').value;
+  var description = document.querySelector(
+    'textarea[name="description"]'
+  ).value;
+  var steps = document.querySelector('textarea[name="steps"]').value;
+  var time_minutes = document.querySelector(
+    'input[name="time_minutes"]'
+  ).value;
+  var name_category = document.querySelector(
+    'input[name="name_category"]:checked'
+  ).value;
+  var ingredients = listadoIngredientes; // Usamos el array listadoIngredientes
+  var pictures = [];
+
+  //valida si esta vacio y si posee datos, agrega las imagenes y las divide en comas
+  if (picturesInput.trim() !== "") {
+    const picturesArray = picturesInput.split(",");
+    const isArray =
+      picturesArray.length > 0 ? picturesArray : picturesInput.split();
+
+    if (isArray.length > 5) {
+      document.getElementById("error").innerHTML =
+        "Error - No debe haber más de 5 imágenes";
+        return;
+    } else {
+      var newPictureArray = createArrayToPicture(picturesArray);
+    }
+  }
+
+  fetch("/api/save-recipe", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      auth: {
+        username: username,
+        password: password,
+      },
+      title: title,
+      description: description,
+      steps: steps,
+      time_minutes: time_minutes,
+      name_category: name_category,
+      ingredients: ingredients,
+      pictures: newPictureArray,
+    }),
+  })
+    .then(function (e) {
+      if (e.ok) {
+        var auth = {
+          username: username,
+          password: password,
+        };
+        var authJSON = JSON.stringify(auth);
+
+        window.alert("Receta publicada");
+        window.location.replace("/recetas");
+      } else {
+        console.log(e);
+        window.alert("No completaste todos los datos");
+      }
+    })
+    .catch(function (e) {
+      console.error(e);
+      //document.getElementById("error").textContent = "Rompio";
+      document.getElementById("error").textContent = "Error en la solicitud: " + error.message;
+    });
+}
+
+function agregarIngrediente() {
+  var ingrediente = document.getElementById("ingrediente").value;
+  var cantidad = document.getElementById("cantidad").value;
+
+  listadoIngredientes.push({ nombre: ingrediente, cantidad: cantidad });
+
+  // Limpiar el contenido actual de "listadoIngredientes".
+  const content = document.getElementById("listadoIngredientes");
+  content.innerHTML = "";
+
+  // Iterar sobre listadoIngredientes y agregar elementos a la lista.
+  listadoIngredientes.forEach(function (ingrediente) {
+    const li = document.createElement("li");
+    li.textContent = `${ingrediente.nombre} ${ingrediente.cantidad}`;
+    content.appendChild(li);
+  });
+
+  // Limpiar los campos de entrada después de agregar los datos.
+  document.getElementById("ingrediente").value = "";
+  document.getElementById("cantidad").value = "";
+}
