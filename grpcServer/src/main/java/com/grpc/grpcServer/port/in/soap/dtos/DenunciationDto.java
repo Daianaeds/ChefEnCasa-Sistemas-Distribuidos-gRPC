331886c3319e9b0535aa49e682212827cef81187
susanavarela <sudava10@gmail.com>
2023-10-22 19:45:29 -0300

    Crear denuncia y listarlas, entidad, dto, repo, service, mapper y controller

---
+++ grpcServer/src/main/java/com/grpc/grpcServer/port/in/soap/dtos/DenunciationDto.java
@@ -0,0 +1,38 @@
+package com.grpc.grpcServer.port.in.soap.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class DenunciationDto {
+
+    @XmlElement
+    private int id;
+
+    @XmlElement
+    private String username;
+
+    @XmlElement
+    private int motive;
+
+    @XmlElement
+    private int idRecipe;
+
+    @XmlElement
+    private String title;
+
+}
