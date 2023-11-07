e9540e953a75223bc3e4445da009cc8b8cef9304
Franco Fraiz <franco99ariel@gmail.com>
2023-11-07 12:07:51 -0300

    Se acomoda el codigo y agregan algunos comentarios

--- Js/mensRecibidos.js
+++ Js/mensRecibidos.js
@@ -15,16 +15,11 @@
         let html = "<div class='container row'>";
 
         obj.forEach((element) => {
-
                 html += "<div class='col-mc-1'><div class='card card-body position-relative' style='width: 18rem;' style= 'display:block'>"
                 html += "<p>Remitente: " + element.destination + "</p>"
                 html += "<p>Mensaje: " + element.subject + "</p>"
                 html += "<div><button class='btn btn-primary ml-auto' onclick='redirectToEnviarMens(\"" + element.destination + "\")' > "
-               // html += "<div><button id='btn-fav-" + element.username + "' class='btn btn-primary ml-auto' onclick='toggleFavoritosEnServidor(\"" + usernameActive + "\",\"" + element.username + "\")' data-username='${element.username}' >" + claseBoton + "</button>"
-                //html += "<button id='btn-enviar-mensaje-" + element.username + "' class='btn btn-primary ml-auto' onclick='redirectToEnviarMens(\"" + element.username + "\")' data-route='/enviarMens'>Enviar mensaje</button>"
-
-                html += "Responder</button></div></div>"
-            
+                html += "Responder</button></div></div>"            
         })
         html += '</div>'
         content.innerHTML = html;
